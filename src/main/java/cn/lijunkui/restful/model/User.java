e18ed9bd885bd24cdcd113776eceb2b5f955c03b
zhuoqianmingyue <ljk126wy@126.com>
2019-01-15 15:57:03 +0800

    添加 hibernate validator 校验示例

--- src/main/java/cn/lijunkui/restful/model/User.java
+++ src/main/java/cn/lijunkui/restful/model/User.java
@@ -1,8 +1,15 @@
 package cn.lijunkui.restful.model;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+
+
 public class User {
-	
+	@NotEmpty(message="请输入您的名称！")
 	private String name;
+	@Max(value = 100, message = "年龄必须在20-100之间！")
+	@Min(value= 20 ,message= "年龄必须在20-100之间！" )
     private Integer age;
  
     public String getName() {
