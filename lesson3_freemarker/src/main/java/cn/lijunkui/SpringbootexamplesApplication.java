722996bf38d2b742a5709e2d3922b54781f4ac3f
zhuoqianmingyue <ljk126wy@126.com>
2019-04-09 17:44:37 +0800

    springboot2.0之整合freemarker示例程序

---
+++ lesson3_freemarker/src/main/java/cn/lijunkui/SpringbootexamplesApplication.java
@@ -0,0 +1,12 @@
+package cn.lijunkui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootexamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootexamplesApplication.class, args);
+	}
+}
