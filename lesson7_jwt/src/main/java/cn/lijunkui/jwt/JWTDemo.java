17cfff57e8905a907ea4cc7a060f1dcec684da99
zhuoqianmingyue <ljk126wy@126.com>
2019-05-15 19:09:48 +0800

    format 一下代码

--- lesson7_jwt/src/main/java/cn/lijunkui/jwt/JWTDemo.java
+++ lesson7_jwt/src/main/java/cn/lijunkui/jwt/JWTDemo.java
@@ -22,6 +22,7 @@ public class JWTDemo {
 		try {
 			String secret = "secret";//token 密钥
 		    Algorithm algorithm = Algorithm.HMAC256("secret");
+		    
 		    //头部信息
 		    Map<String, Object> map = new HashMap<String, Object>();
 		    map.put("alg", "HS256");
@@ -51,25 +52,27 @@ public class JWTDemo {
 	public String createTokenWithClaim() {
 		
 		try {
-		    Algorithm algorithm = Algorithm.HMAC256("secret");
+			Date nowDate = new Date();
+			Date expireDate = getAfterDate(nowDate,0,0,0,2,0,0);//2小过期
+			
 		    Map<String, Object> map = new HashMap<String, Object>();
-		    Date nowDate = new Date();
-		    Date expireDate = getAfterDate(nowDate,0,0,0,2,0,0);//2小过期
 	        map.put("alg", "HS256");
 	        map.put("typ", "JWT");
+	       
+		    Algorithm algorithm = Algorithm.HMAC256("secret");
 		    String token = JWT.create()
-		    	/*设置头部信息 Header*/
 		    	.withHeader(map)
 		    	/*设置 载荷 Payload*/
-		    	.withClaim("loginName", "lijunkui")
+		    	.withClaim("loginName", "zhuoqianmingyue")
 		        .withIssuer("SERVICE")//签名是有谁生成 例如 服务器
 		        .withSubject("this is test token")//签名的主题
-		        //.withNotBefore(new Date())//定义在什么时间之前，该jwt都是不可用的.
+		        //.withNotBefore(new Date())//该jwt都是不可用的时间
 		        .withAudience("APP")//签名的观众 也可以理解谁接受签名的
 		        .withIssuedAt(nowDate) //生成签名的时间
 		        .withExpiresAt(expireDate)//签名过期的时间
 		        /*签名 Signature */
 		        .sign(algorithm);
+		    
 		    return token;
 		} catch (JWTCreationException exception){
 			exception.printStackTrace();
@@ -121,12 +124,14 @@ public class JWTDemo {
 		    JWTVerifier verifier = JWT.require(algorithm)
 		        .withIssuer("SERVICE")
 		        .build(); //Reusable verifier instance
+		    
 		    DecodedJWT jwt = verifier.verify(token);
 		    String subject = jwt.getSubject();
+		    List<String> audience = jwt.getAudience();
 		    Map<String, Claim> claims = jwt.getClaims();
+		    
 		    Claim claim = claims.get("loginName");
 		    System.out.println(claim.asString());
-		    List<String> audience = jwt.getAudience();
 		    System.out.println(subject);
 		    System.out.println(audience.get(0));
 		} catch (JWTVerificationException exception){
