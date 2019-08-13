83fc48b6ed7812dedac00978d2b39f8ced84c41a
zhuoqianmingyue <ljk126wy@126.com>
2019-08-13 17:19:03 +0800

    格式化一下代码

--- spring-boot-2.x-sentinel/src/main/java/cn/lijunkui/test/TestController.java
+++ spring-boot-2.x-sentinel/src/main/java/cn/lijunkui/test/TestController.java
@@ -2,9 +2,8 @@ package cn.lijunkui.test;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.alibaba.csp.sentinel.annotation.SentinelResource;
-//https://blog.csdn.net/yangliuhbhd/article/details/89948169
+
 @RestController
 public class TestController {
  	@GetMapping(value = "/hello")
