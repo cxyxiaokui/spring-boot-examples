da96ec621f8a53a34ffd7b17c431aec2b8485984
zhuoqianmingyue <ljk126wy@126.com>
2019-08-13 16:35:50 +0800

    修改和本地冲突

--- lesson20_rabbitMQ/src/main/java/cn/lijunkui/rabbitmq/topic/controller/RabbitMQTopicDemoController.java
+++ lesson20_rabbitMQ/src/main/java/cn/lijunkui/rabbitmq/topic/controller/RabbitMQTopicDemoController.java
@@ -4,8 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import cn.lijunkui.rabbitmq.helloworld.RabbitMQSend;
 import cn.lijunkui.rabbitmq.topic.RabbitMQTopicSend;
 
 @RestController
