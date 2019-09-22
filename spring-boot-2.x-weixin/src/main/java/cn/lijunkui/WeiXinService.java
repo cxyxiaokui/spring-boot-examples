5a677449091f0c714ee3b37254558f375cdc7615
zhuoqianmingyue <ljk126wy@126.com>
2019-09-22 10:21:57 +0800

    修改部分代码

--- spring-boot-2.x-weixin/src/main/java/cn/lijunkui/WeiXinService.java
+++ spring-boot-2.x-weixin/src/main/java/cn/lijunkui/WeiXinService.java
@@ -1,11 +1,15 @@
 package cn.lijunkui;
 
+import java.security.MessageDigest;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
@@ -14,80 +18,64 @@ import cn.lijunkui.config.WeiXinConfig;
 @Component
 public class WeiXinService {
 	
-	private static final String AUTHORIZEURL="https://open.weixin.qq.com/connect/oauth2/authorize";
-	private static final String GE_TACCESSTOKEN_URL = "https://api.weixin.qq.com/sns/oauth2/access_token";
-	private static final String GE_USERINFO_URL = "https://api.weixin.qq.com/sns/userinfo";
+	private static final String GETWEIXINUSERINFO_AUTHORIZE="https://open.weixin.qq.com/connect/oauth2/authorize";
+	private static final String GETWEIXINUSERINFO_GETACCESSTOKEN = "https://api.weixin.qq.com/sns/oauth2/access_token";
+	private static final String GETWEIXINUSERINFO_GETUSERINFO = "https://api.weixin.qq.com/sns/userinfo";
+	private static final String JSSDK_ACCESSTOKEN = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=APPID&secret=APPSECRET";
+	private static final String JSSDK_GETTICKET = "https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=ACCESS_TOKEN&type=jsapi";
 	
 	@Autowired
 	private  WeiXinConfig weiXinConfig;
 	@Autowired
 	private RestTemplate restTemplate;
+	
 	/**
-	 * 拼接用户授权重定向的URL
+	 * 构建引导用户授权重定向的URL
 	 * @param url
 	 * @return
 	 */
 	public String buildAuthorizeURL(String url){
-		
 		return concatAuthorizeURL(url);
 	}
-	/**
-	 * 获取微信用户的Opid
-	 * @param code
-	 * @return
-	 */
-	public Map<String,Object> getOpenIdInfo(String code){
-		
-		String getAccessTokenUrl = concatGetOpenIdInfoURL(code);
-		String json = postRequestForWechat(getAccessTokenUrl);
-		Map<String,Object> map = jsonToMap(json);
-		
-		return map;
-	}
 	
 	/**
-	 * 获取用户信息
-	 * @param map
+	 * 拼接引导用户授权重定向的URL
+	 * snsapi_base （不弹出授权页面，直接跳转，只能获取用户openid）
+	 * snsapi_userinfo （弹出授权页面，可通过openid拿到昵称、性别、所在地。并且， 即使在未关注的情况下，只要用户授权，也能获取其信息 ）
+	 * @param url
 	 * @return
 	 */
-	public Map getWeiXinUserInfo(Map<String, Object> map) {
-		
-		String getUserInfoUrl = concatGetWeiXinUserInfoURL(map);
-		String json = getRequestForWechat(getUserInfoUrl);
-		Map userInfoMap = jsonToMap(json);
-		
-		return userInfoMap;
-	}
-	
 	private String concatAuthorizeURL(String url) {
-		StringBuilder authorizeUrl = new StringBuilder(AUTHORIZEURL);
+		StringBuilder authorizeUrl = new StringBuilder(GETWEIXINUSERINFO_AUTHORIZE);
 		authorizeUrl.append("?appid=").append(weiXinConfig.getAppID());
 		authorizeUrl.append("&redirect_uri=").append(url);
 		authorizeUrl.append("&response_type=code");
-		 //snsapi_base （不弹出授权页面，直接跳转，只能获取用户openid），
-		 //snsapi_userinfo （弹出授权页面，可通过openid拿到昵称、性别、所在地。并且， 即使在未关注的情况下，只要用户授权，也能获取其信息 ）
 		authorizeUrl.append("&scope=snsapi_userinfo");
 		authorizeUrl.append("&state=").append("STATE");
 		authorizeUrl.append("#wechat_redirect");
 		return authorizeUrl.toString();
 	}
-
 	
-	
-	private Map jsonToMap(String json) {
-		Gson gons = new Gson();
-		Map map = gons.fromJson(json, new TypeToken<Map>(){}.getType());
+	/**
+	 * 获取微信用户的 accesstoken 和 openid
+	 * @param code
+	 * @return
+	 */
+	public Map<String,Object> getACCESSTOKEN(String code){
+		
+		String getAccessTokenUrl = concatGetAccessTokenInfoURL(code);
+		String json = postRequestForWeiXinService(getAccessTokenUrl);
+		Map<String,Object> map = jsonToMap(json);
+		
 		return map;
 	}
-
-	private String postRequestForWechat(String getAccessTokenUrl) {
-		ResponseEntity<String> postForEntity = restTemplate.postForEntity(getAccessTokenUrl, null, String.class);
-		String json = postForEntity.getBody();
-		return json;
-	}
-
-	private String concatGetOpenIdInfoURL(String code) {
-		StringBuilder getAccessTokenUrl = new StringBuilder(GE_TACCESSTOKEN_URL);
+	/**
+	 * 拼接调用微信服务获取  accesstoken 和 openid URL
+	 * @param code
+	 * @return
+	 */
+	private String concatGetAccessTokenInfoURL(String code) {
+		StringBuilder getAccessTokenUrl = new StringBuilder(GETWEIXINUSERINFO_GETACCESSTOKEN);
 		getAccessTokenUrl.append("?appid=").append(weiXinConfig.getAppID());
 		getAccessTokenUrl.append("&secret=").append(weiXinConfig.getAppsecret());
 		getAccessTokenUrl.append("&code=").append(code);
@@ -95,21 +83,164 @@ public class WeiXinService {
 		return getAccessTokenUrl.toString();
 	}
 
-	private String getRequestForWechat(String getUserInfoUrl) {
-		ResponseEntity<String> postForEntity = restTemplate.getForEntity(getUserInfoUrl.toString(), String.class);
-		String json = postForEntity.getBody();
-		return json;
+	/**
+	 * 获取微信用户信息
+	 * @param map
+	 * @return
+	 */
+	public Map getWeiXinUserInfo(Map<String, Object> map) {
+		
+		String getUserInfoUrl = concatGetWeiXinUserInfoURL(map);
+		String json = getRequestForWeiXinService(getUserInfoUrl);
+		Map userInfoMap = jsonToMap(json);
+		
+		return userInfoMap;
 	}
-
+	/**
+	 * 拼接调用微信服务获取用户信息的URL
+	 * @param map
+	 * @return
+	 */
 	private String concatGetWeiXinUserInfoURL(Map<String, Object> map) {
 		String openId = (String) map.get("openid");
 		String access_token = (String) map.get("access_token");
 		// 检验授权凭证（access_token）是否有效
-		StringBuilder getUserInfoUrl = new StringBuilder(GE_USERINFO_URL);
+		StringBuilder getUserInfoUrl = new StringBuilder(GETWEIXINUSERINFO_GETUSERINFO);
 		getUserInfoUrl.append("?access_token=").append(access_token);
 		getUserInfoUrl.append("&openId=").append(openId);
 		getUserInfoUrl.append("&lang=zh_CN");
 		
 		return getUserInfoUrl.toString();
+	}
+	/**
+	 * 初始化JSSDK配置信息
+	 * @param shareUrl
+	 * @return
+	 * @throws Exception
+	 */
+	public Map initJSSDKConfigInfo(String shareUrl) throws Exception {
+		
+		String accessToken = this.getJSSDKAccessToken();
+		String jsapiTicket = this.getJSSDKJsapiTicket(accessToken);
+		String timestamp = Long.toString(System.currentTimeMillis() / 1000);
+		String nonceStr = UUID.randomUUID().toString();
+		String signature = this.buildJSSDKSignature(jsapiTicket,timestamp,nonceStr,shareUrl);
+		
+		Map<String,String> map = new HashMap<String,String>();
+		map.put("shareUrl", shareUrl);
+		map.put("jsapi_ticket", jsapiTicket);
+		map.put("nonceStr", nonceStr);
+		map.put("timestamp", timestamp);
+		map.put("signature", signature);
+		map.put("appid", weiXinConfig.getAppID());
+		return map;
+	}
+	public String getJSSDKAccessToken() {
+		String token = null;
+		String url = JSSDK_ACCESSTOKEN.replaceAll("APPID",
+				weiXinConfig.getAppID()).replaceAll("APPSECRET",
+				weiXinConfig.getAppsecret());
+
+		String json = postRequestForWeiXinService(url);
+		Map map = jsonToMap(json);
+		if (map != null) {
+			token = (String) map.get("access_token");
+		}
+		return token;
+	}
+	
+	public String getJSSDKJsapiTicket(String token) {
+	  String url = JSSDK_GETTICKET.replaceAll("ACCESS_TOKEN", token);
+	  String json = postRequestForWeiXinService(url);
+	  Map map = jsonToMap(json);
+	  String jsapi_ticket = null;
+	  if (map != null) {
+          jsapi_ticket = (String) map.get("ticket");
+      }
+	  return jsapi_ticket;
+	}
+	
+	 /**
+     * 构建分享链接的签名。
+     * @param ticket
+     * @param nonceStr
+     * @param timeStamp
+     * @param url
+     * @return
+     * @throws Exception
+     */
+    public static String buildJSSDKSignature(String ticket,String timestamp,String nonceStr ,String url) throws Exception {
+    	
+    	String orderedString = "jsapi_ticket=" + ticket
+                + "&noncestr=" + nonceStr + "&timestamp=" + timestamp
+                + "&url=" + url;
+        return sha1(orderedString);
+    }
+    
+    /**
+     * sha1 加密JSSDK微信配置参数获取签名。
+     * 
+     * @param signature
+     * @param timestamp
+     * @param nonce
+     * @return
+     */
+    public static String sha1(String orderedString) throws Exception {
+        String ciphertext = null;
+        MessageDigest md = MessageDigest.getInstance("SHA-1");
+        byte[] digest = md.digest(orderedString.getBytes());
+        ciphertext = byteToStr(digest);
+        return ciphertext.toLowerCase();
+    }
+    /** 
+     * 将字节数组转换为十六进制字符串 
+     *  
+     * @param byteArray 
+     * @return 
+     */ 
+    private static String byteToStr(byte[] byteArray) {  
+        String strDigest = "";  
+        for (int i = 0; i < byteArray.length; i++) {  
+            strDigest += byteToHexStr(byteArray[i]);  
+        }  
+        return strDigest;  
+    }  
+    /** 
+     * 将字节转换为十六进制字符串 
+     *  
+     * @param mByte 
+     * @return 
+     */ 
+    private static String byteToHexStr(byte mByte) {  
+        char[] Digit = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };  
+        char[] tempArr = new char[2];  
+        tempArr[0] = Digit[(mByte >>> 4) & 0X0F];  
+        tempArr[1] = Digit[mByte & 0X0F];  
+
+        String s = new String(tempArr);  
+        return s;  
+    }
+    
+	public String mapToJson(Map map){
+		Gson gson = new Gson();
+		String json = gson.toJson(map);
+		return json;
+	}
+	private Map jsonToMap(String json) {
+		Gson gons = new Gson();
+		Map map = gons.fromJson(json, new TypeToken<Map>(){}.getType());
+		return map;
+	}
+
+	private String postRequestForWeiXinService(String getAccessTokenUrl) {
+		ResponseEntity<String> postForEntity = restTemplate.postForEntity(getAccessTokenUrl, null, String.class);
+		String json = postForEntity.getBody();
+		return json;
+	}
+	
+	private String getRequestForWeiXinService(String getUserInfoUrl) {
+		ResponseEntity<String> postForEntity = restTemplate.getForEntity(getUserInfoUrl.toString(), String.class);
+		String json = postForEntity.getBody();
+		return json;
 	}
 }
--- spring-boot-2.x-weixin/src/main/java/cn/lijunkui/controller/WeiXinDemoController.java
+++ spring-boot-2.x-weixin/src/main/java/cn/lijunkui/controller/WeiXinDemoController.java
@@ -2,23 +2,42 @@ package cn.lijunkui.controller;
 
 import java.io.IOException;
 import java.net.URLEncoder;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.google.gson.Gson;
+
 import cn.lijunkui.WeiXinService;
+import cn.lijunkui.config.WeiXinConfig;
 
 
 @RestController
 @RequestMapping("/weixin")
 public class WeiXinDemoController {
+	
 	@Autowired
 	private WeiXinService weiXinService;
-
+	@Autowired
+	private  WeiXinConfig weiXinConfig;
+	/**
+	 * 获取微信用户信息
+	 * @param code
+	 * @param request
+	 * @param response
+	 * @param session
+	 * @return
+	 * @throws IOException
+	 */
 	@RequestMapping("/getWeiXinUserInfo")
 	public String getWeiXinUserInfo(String code,HttpServletRequest request,HttpServletResponse response,HttpSession session) throws IOException{
 		//第一步：用户同意授权，获取code
@@ -30,17 +49,22 @@ public class WeiXinDemoController {
 		}
 		//第二步：通过code换取网页授权access_token
 		String htmlInfo = "";
-		Map<String, Object> openIdInfo = weiXinService.getOpenIdInfo(code);
-		String errcode = (String)openIdInfo.get("errcode");
+		Map<String, Object> accesstokenInfo = weiXinService.getACCESSTOKEN(code);
+		String errcode = (String)accesstokenInfo.get("errcode");
 		if(StringUtils.isEmpty(errcode)){
 			//第四步：拉取用户信息(需scope为 snsapi_userinfo)
-			Map<String, Object> weiXinUserInfo = weiXinService.getWeiXinUserInfo(openIdInfo);
+			Map<String, Object> weiXinUserInfo = weiXinService.getWeiXinUserInfo(accesstokenInfo);
 			String userInfohtml = createUserInfoHtml(weiXinUserInfo);
 			return userInfohtml;
 		}
 		return htmlInfo;
 	}
-
+	
+	/**
+	 * 生成微信用户信息Html
+	 * @param weiXinUserInfo
+	 * @return
+	 */
 	private String createUserInfoHtml(Map<String, Object> weiXinUserInfo) {
 		String wxOpenId = (String)weiXinUserInfo.get("openid");
 		String nickname = (String)weiXinUserInfo.get("nickname");
@@ -75,4 +99,20 @@ public class WeiXinDemoController {
 		"</html>";
 		return htmlInfo;
 	}
+	
+	/**
+	 * 初始化微信JSSDK配置信息
+	 * @param request
+	 * @param response
+	 * @return
+	 * @throws Exception
+	 */
+	@RequestMapping("/initWXJSSDKConfigInfo")
+	public String initWXJSConfig (HttpServletRequest request,HttpServletResponse response) throws Exception{
+		String shareUrl = request.getParameter("shareUrl");//分享的URL
+		Map map = weiXinService.initJSSDKConfigInfo(shareUrl);
+		String json = weiXinService.mapToJson(map);
+		return json;
+	}
+	
 }
