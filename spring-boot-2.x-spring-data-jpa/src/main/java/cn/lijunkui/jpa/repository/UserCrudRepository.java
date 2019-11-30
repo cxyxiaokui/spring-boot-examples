8db1d327347f012186f329c6c3c2f2940b01ce53
zhuoqianmingyue <ljk126wy@126.com>
2019-11-30 16:34:10 +0800

    添加计数删除和查询方法

--- spring-boot-2.x-spring-data-jpa/src/main/java/cn/lijunkui/jpa/repository/UserCrudRepository.java
+++ spring-boot-2.x-spring-data-jpa/src/main/java/cn/lijunkui/jpa/repository/UserCrudRepository.java
@@ -1,9 +1,16 @@
 package cn.lijunkui.jpa.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import cn.lijunkui.jpa.model.User;
 
 public interface UserCrudRepository extends CrudRepository<User,Long>{
-
+	long countByName(String name);
+	@Transactional
+	List<User> reomveByName(String name);
+	@Transactional
+	List<User> deleteByName(String name);
 }
